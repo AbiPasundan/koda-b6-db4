5dfd07383bf8db2659d19a72d9c0bf7cb0c32c3f
AbiPasundan <muhamad11wildan4n@gmail.com>
2026-02-24 11:32:17 +0700

    feat: update base.sql make relation for every entity

--- README.md
+++ README.md
@@ -9,22 +9,30 @@ title: Authentication Flow
 erDiagram
 
     User {
-        id int pk
+        user_id int pk
         name varchar
         email varchar
         password varchar
         phone varchar
         address text
         photo varchar
-        role enum fk
+        role varchar fk
         created_at time_stamp
     }
-    Role ||--|{ User : ""
+
+    Role }|--|| User_Role : ""
     Role {
         role_id int pk
         admin varchar
         user varchar
     }
+    User_Role ||--|{User : ""
+    User_Role {
+        uesr_role_id int pk
+        uesr_id int fk
+        role_id int fk
+    }
+    
     User ||--|{ Forgot_Password : ""
     Forgot_Password {
         id int pk
--- base.sql
+++ base.sql
@@ -1,5 +1,6 @@
-CREATE TYPE ROLE AS ENUM ('admin', 'user');
-CREATE TABLE "user" (
+-- drop table "user", "role";
+
+CREATE TABLE "user_library" (
     "id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
     "name" VARCHAR(30),
     "email" VARCHAR(100),
@@ -7,6 +8,25 @@ CREATE TABLE "user" (
     "phone" VARCHAR(30),
     "address" TEXT,
     "photo" VARCHAR(30),
-    "role" ROLE,
+    "user_role" VARCHAR(30),
     "created_at" TIMESTAMP
 );
+
+CREATE TABLE "roles" (
+    "role_id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
+    "admin" VARCHAR(30) UNIQUE
+    "user" VARCHAR(30) UNIQUE
+);
+CREATE TABLE "user_roles" (
+    "user_role_id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
+    "user_id" int UNIQUE
+    "role_id" int UNIQUE
+);
+
+CREATE TABLE "forgot_password" (
+    "forgot_password_id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY UNIQUE,
+    "user_id" INT UNIQUE
+);
+
+ALTER TABLE "user" ADD CONSTRAINT fk_role FOREIGN KEY (user_role) REFERENCES role(name);
+ALTER TABLE "forgot_password" ADD CONSTRAINT fk_password FOREIGN KEY (user_password) REFERENCES name(password);
